15fb1b0cf7d4e44fd33524add8393e70ce5f3970
Arcticfox712 <Pragyasrivastava@gmail.com>
2025-07-28 18:53:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print ("hello world!")
+import pgzrun
+HEIGHT=500
+WIDTH=500
+
+def draw():
+    r=250
+    g=100
+    b=150
+    width=WIDTH-100
+    hight=HEIGHT-150
+
+    rect=Rect((0,0), (width, hight))
+    rect.center=250,250
+    screen.draw.rect(rect,(r,g,b))
+
+pgzrun.go()
+
+    
+
+
+
+
+
+
+
+
+
+
+
